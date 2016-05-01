b98f80bd7ff9077be514338027bf05496fee595c
unbornchikken <gabor.mezo@outlook.com>
2016-05-01 15:36:49 +0200

    synth test fixes

--- test/synthesizer.jl
+++ test/synthesizer.jl
@@ -2,12 +2,12 @@ testOnAllContexts("Synthesizer") do ctx
     println("\tconcept: select indices by chance")
     const depth = 2
     const count = 4
-    s = range(ctx, Float32, count, 0)
+    s = range(ctx, Float32, [count])
     dist = randu(ctx, Float32, count, depth)
     rnd = randu(ctx, Float32, count, depth)
-    test = rnd ./ max(dist, 0.000001f0)
+    test = rnd ./ maxOf(dist, 0.000001f0)
     idx, max = imax(test, 1)
-    idx2 = idx ./ count + s
+    idx2 = idx ./ count .+ s
     result = dist[idx2]
     @test dims(result, 0) == count
     @test dims(result, 1) == 1
